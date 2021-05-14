4edce8e7d8eacb8cf783188b0c166d1df43ab5c9
Chatmalow <maxime.pie.mail@gmail.com>
2021-05-14 12:18:59 +0200

    doc: Add Readme file with basic info

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,6 +3,9 @@ import ReactStopwatch from 'react-stopwatch';
 
 const Stopwatch = () => (
   <ReactStopwatch
+    hours={0}
+    minutes={0}
+    seconds={0}
     render={({ formatted}) => {
       return (
         <span>
