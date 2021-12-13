a8c204866421f2edb2062bfd9b04ca1141c48594
MaximePie <maxime.pie.mail@gmail.com>
2021-12-13 09:12:34 +0100

    fix: Temporarly remove streak effect to see if we memorize better

--- src/components/molecules/Cards.js
+++ src/components/molecules/Cards.js
@@ -50,8 +50,8 @@ export default function Cards({cardsList, triggerCardUpdate, remainingCards}) {
 
 
   /**
-   * If the answer is wrong, we go back to the first interval : 15 seconds
-   * If the answer is right, we increment de interval so it appears later
+   * If the answer is wrong, we go back to the previous interval so it appears earlier
+   * If the answer is right, we increment the interval so it appears later
    * @param cardId
    * @param isSuccess
    */
@@ -60,12 +60,14 @@ export default function Cards({cardsList, triggerCardUpdate, remainingCards}) {
     const targetCard = cardsList.find((card) => card._id === cardId);
 
     let updatedCard = {...targetCard};
+    const {currentDelay} = updatedCard
     const currentDelayIndex = intervals.indexOf(updatedCard.currentDelay);
+    const shouldIncreaseDelay = isSuccess && currentDelayIndex !== 0
 
     // Edit data
-    if (!updatedCard.currentDelay) {
+    if (!currentDelay) {
       updatedCard.currentDelay = intervals[1];
-    } else if (isSuccess && currentDelayIndex !== 0) {
+    } else if (shouldIncreaseDelay) {
       let newDelayIndex = currentDelayIndex;
       if (updatedCard.currentSuccessfulAnswerStreak) {
         newDelayIndex += updatedCard.currentSuccessfulAnswerStreak;
@@ -73,18 +75,35 @@ export default function Cards({cardsList, triggerCardUpdate, remainingCards}) {
       else {
         newDelayIndex += 1;
       }
-      updatedCard.currentDelay = intervals[newDelayIndex];
-      if (updatedCard.currentSuccessfulAnswerStreak > 2) {
-        store.addNotification({
-          ...streakNotification,
-          message: `${updatedCard.currentSuccessfulAnswerStreak} à la suite !`,
-        });
-      }
+
+      // TODO - Remove this line to enable streak effect
+      newDelayIndex = currentDelayIndex + 1;
+
+      updatedCard.currentDelay = intervals[newDelayIndex]
+
+      tryToDisplayStreakNotification();
     } else {
       updatedCard.currentDelay = intervals[currentDelayIndex - 1];
     }
 
     triggerCardUpdate(updatedCard);
+  }
+
+  /**
+   * If the parameter >= 3,
+   * Display the current successful answer streak notification
+   * Else, do nothing
+   * @param currentSuccessfulAnswerStreak Number, the current amount of successful answers
+   * the user gave.
+   */
+  function tryToDisplayStreakNotification(currentSuccessfulAnswerStreak) {
+    const shouldDisplayStreakNotification = currentSuccessfulAnswerStreak >= 3
+    if (shouldDisplayStreakNotification) {
+      store.addNotification({
+        ...streakNotification,
+        message: `${currentSuccessfulAnswerStreak} à la suite !`,
+      });
+    }
   }
 
 }
