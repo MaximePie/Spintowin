55642f8a554335de01bed1c147ec2115dc9e6238
Chatmalow <maxime.pie.mail@gmail.com>
2022-04-07 15:41:41 +0200

    refactor: Use JSX extension

--- src/components/molecules/LoadingAppGif.jsx
+++ src/components/molecules/LoadingAppGif.jsx
@@ -6,8 +6,8 @@ export default function LoadingAppGif() {
     return (
         <div className="LoadingAppGif">
           <div className="LoadingAppGif__container">
-            <img className="LoadingAppGif__image" src={sleepingCastor} alt="Image de castor"/>
-            <img className="LoadingAppGif__loadingGif" src={loadingGif} alt="Gif de chargement"/>
+            <img className="LoadingAppGif__image" src={sleepingCastor} alt="castor"/>
+            <img className="LoadingAppGif__loadingGif" src={loadingGif} alt="chargement"/>
             <p>Réveil de la famille Castor</p>
           </div>
         </div>
