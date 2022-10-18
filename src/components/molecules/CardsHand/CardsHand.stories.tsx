8365cb6323f1294ea14e38a8ab4aa2328e7bff77
Maxime Pie <maxime.pie.mail@gmail.com>
2022-10-18 06:40:56 +0200

    fix: add sound activation in user stories

--- src/components/molecules/CardsHand/CardsHand.stories.tsx
+++ src/components/molecules/CardsHand/CardsHand.stories.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
-import CardsHand from "./CardsHand";
-import CardsHandDisplay from "./CardsHandDisplay";
-import UserCard from "../../../types/UserCard";
-import {ObjectID} from "bson";
-import {UserContext, UserContextType} from "../../../contexts/user";
-import User from "../../../types/User";
-
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ObjectID } from 'bson';
+import CardsHand from './CardsHand';
+import CardsHandDisplay from './CardsHandDisplay';
+import UserCard from '../../../types/UserCard';
+import { UserContext, UserContextType } from '../../../contexts/user';
+import User from '../../../types/User';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -14,29 +13,25 @@ export default {
   component: CardsHand,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: {control: 'color'},
+    backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof CardsHand>;
 
-
-
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof CardsHand> = function Template(args) {
-  const cards: UserCard[] = [1,2,3,4,5].map(cardNumber => {
-    return {
-      question: `Question ${cardNumber}`,
-      image: '',
-      currentDelay: 5,
-      cardId: new ObjectID(),
-      answer: `Réponse ${cardNumber}`,
-      isAssignedToConnectedUser: true,
-      category: '',
-      currentSuccessfulAnswerStreak: 1,
-      isOwnerOfCard: true,
-      _id: new ObjectID(),
-      isMemorized: false,
-    }
-  })
+const Template: ComponentStory<typeof CardsHand> = function Template(_args) {
+  const cards: UserCard[] = [1, 2, 3, 4, 5].map((cardNumber) => ({
+    question: `Question ${cardNumber}`,
+    image: '',
+    currentDelay: 5,
+    cardId: new ObjectID(),
+    answer: `Réponse ${cardNumber}`,
+    isAssignedToConnectedUser: true,
+    category: '',
+    currentSuccessfulAnswerStreak: 1,
+    isOwnerOfCard: true,
+    _id: new ObjectID(),
+    isMemorized: false,
+  }));
 
   const user: UserContextType = {
     user: {} as User,
@@ -47,14 +42,14 @@ const Template: ComponentStory<typeof CardsHand> = function Template(args) {
     updateInterval: () => {},
     selectedCategory: null,
     intervals: [],
-  }
+    setSoundActivation: () => {},
+  };
 
   return (
     <UserContext.Provider value={user}>
-      <CardsHandDisplay cards={cards}/>
+      <CardsHandDisplay cards={cards} onFail={() => {}} />
     </UserContext.Provider>
   );
-}
+};
 
 export const Default = Template.bind({});
-
--- src/components/molecules/SettingsModal/SettingsModal.stories.tsx
+++ src/components/molecules/SettingsModal/SettingsModal.stories.tsx
@@ -1,12 +1,12 @@
-import React, {ChangeEvent, useContext, useState} from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
+import React, { ChangeEvent, useContext, useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ObjectId } from 'bson';
 import SettingsModal from './SettingsModal';
-import {UserContext, UserContextProvider} from "../../../contexts/user";
-import Button from "../../atoms/Button/Button";
-import {ObjectId} from "bson";
-import SettingsModalDisplay from "./SettingsModalDisplay";
-import intervals from "../../../data/cards";
-import UserInterval from "../../../types/UserInterval";
+import { UserContext, UserContextProvider } from '../../../contexts/user';
+import Button from '../../atoms/Button/Button';
+import SettingsModalDisplay from './SettingsModalDisplay';
+import intervals from '../../../data/cards';
+import UserInterval from '../../../types/UserInterval';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -14,14 +14,13 @@ export default {
   component: SettingsModal,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: {control: 'color'},
+    backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof SettingsModal>;
 
 type LoginControlsProps = {areCategoriesEnabled?: boolean, intervals: UserInterval[]}
-function LoginControls({areCategoriesEnabled, intervals}: LoginControlsProps) {
-
-  const {setUser} = useContext(UserContext);
+function LoginControls({ areCategoriesEnabled, intervals }: LoginControlsProps) {
+  const { setUser } = useContext(UserContext);
 
   return (
     <Button
@@ -29,7 +28,7 @@ function LoginControls({areCategoriesEnabled, intervals}: LoginControlsProps) {
       onClick={fillUser}
       variant="primary"
     />
-  )
+  );
 
   function fillUser() {
     setUser({
@@ -38,32 +37,41 @@ function LoginControls({areCategoriesEnabled, intervals}: LoginControlsProps) {
       hasStreakNotifications: false,
       experience: 0,
       level: 0,
-      username: "Storybook",
+      username: 'Storybook',
       hasCategoriesDisplayed: areCategoriesEnabled,
-      role: "admin",
+      role: 'admin',
     });
   }
 }
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof SettingsModalDisplay> = function Template(args) {
+const Template: ComponentStory<typeof SettingsModalDisplay> = function Template(_args) {
   const [hasCategoriesDisplayed, setCategoryDisplayState] = useState(false);
   const [hasStreakEnabled, setStreakDisplay] = useState(false);
-  const formattedIntervals = intervals.map(interval => ({value: interval, _id: new ObjectId, isEnabled: true}));
+  const [hasSoundEnabled, setSoundActivationState] = useState(false);
+  const formattedIntervals = intervals.map((interval) => ({ value: interval, _id: new ObjectId(), isEnabled: true }));
   return (
     <UserContextProvider>
-      <LoginControls areCategoriesEnabled={hasCategoriesDisplayed} intervals={formattedIntervals}/>
+      <LoginControls areCategoriesEnabled={hasCategoriesDisplayed} intervals={formattedIntervals} />
       <SettingsModalDisplay
         onIntervalUpdate={() => {}}
         intervals={formattedIntervals}
-        onCategoryDisplayChange={(event: ChangeEvent<HTMLInputElement>) => setCategoryDisplayState(event.target.checked)}
-        onStreakDisplayChange={(event: ChangeEvent<HTMLInputElement>) => setStreakDisplay(event.target.checked)}
+        onCategoryDisplayChange={
+        (event: ChangeEvent<HTMLInputElement>) => setCategoryDisplayState(event.target.checked)
+      }
+        onSoundActivationChange={
+        (event: ChangeEvent<HTMLInputElement>) => setSoundActivationState(event.target.checked)
+      }
+        onStreakDisplayChange={
+        (event: ChangeEvent<HTMLInputElement>) => setStreakDisplay(event.target.checked)
+      }
         hasCategoriesDisplayed={hasCategoriesDisplayed}
         hasStreakEnabled={hasStreakEnabled}
+        hasSoundEnabled={hasSoundEnabled}
         onClose={() => {}}
       />
     </UserContextProvider>
   );
-}
+};
 
 export const Default = Template.bind({});
