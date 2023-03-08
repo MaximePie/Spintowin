cb55d36e955ab5f4a42db49bfe04f13de471aae7
Maxime Pie <maxime.pie.mail@gmail.com>
2023-03-08 10:29:36 +0100

    feat: update question edition modal responsive design

--- src/components/molecules/QuestionEditionModal/QuestionEditionModal.tsx
+++ src/components/molecules/QuestionEditionModal/QuestionEditionModal.tsx
@@ -46,6 +46,9 @@ export default function QuestionEditionModal(props: QuestionEditionModalProps) {
           classNames="QuestionEditionModal"
         >
           <div className="QuestionEditionModal">
+            <h2>
+              {isOwnerOfCard ? 'Modifier la carte' : 'Voir la carte'}
+            </h2>
             <div
               className="QuestionEditionModal__close-button"
               role="button"
@@ -72,43 +75,36 @@ export default function QuestionEditionModal(props: QuestionEditionModalProps) {
               </div>
             )}
             {isOwnerOfCard && (
-              <>
-                <Button
-                  onClick={deleteCard}
-                  type="button"
-                  variant="danger"
-                  icon="fas fa-times"
-                  text="Supprimer"
-                />
-                <div className="QuestionEditionModal__form" onSubmit={save}>
-                  <div className="QuestionEditionModal__fields">
-                    <div>
-                      <div className="QuestionEditionModal__field">
-                        <label>
-                          Question
-                          <InputGroup
-                            type="text"
-                            value={question || ''}
-                            onChange={updateQuestion}
-                            icon={faQuestion}
-                            isIconSolid
-                            placeholder="Quelle est la taille d'un castor ?"
-                          />
-                        </label>
-                      </div>
-                      <div>
-                        <label>
-                          Réponse
-                          <InputGroup
-                            type="text"
-                            value={answer}
-                            onChange={updateAnswer}
-                            icon={faLightbulb}
-                            isIconSolid
-                            placeholder="80 - 100 cm"
-                          />
-                        </label>
-                      </div>
+              <div className="QuestionEditionModal__form" onSubmit={save}>
+                <div className="QuestionEditionModal__fields">
+                  <div>
+                    <div className="QuestionEditionModal__field">
+                      <label>
+                        Question
+                        <InputGroup
+                          type="text"
+                          value={question || ''}
+                          onChange={updateQuestion}
+                          icon={faQuestion}
+                          isIconSolid
+                          placeholder="Quelle est la taille d'un castor ?"
+                        />
+                      </label>
+                    </div>
+                    <div className="QuestionEditionModal__field">
+                      <label>
+                        Réponse
+                        <InputGroup
+                          type="text"
+                          value={answer}
+                          onChange={updateAnswer}
+                          icon={faLightbulb}
+                          isIconSolid
+                          placeholder="80 - 100 cm"
+                        />
+                      </label>
+                    </div>
+                    <div className="QuestionEditionModal__field">
                       <label htmlFor="">
                         Petit indice ?
                         <InputGroup
@@ -119,25 +115,40 @@ export default function QuestionEditionModal(props: QuestionEditionModalProps) {
                         />
                       </label>
                     </div>
-                    {hints && (
-                      <div className="QuestionEditionModal__hints">
-                        <h4>Indices</h4>
-                        <ul>
-                          {hints?.map((existingHint) => (
-                            <li key={existingHint}>
-                              {existingHint}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
                   </div>
-                  <div className="QuestionEditionModal__actions">
+                  <div className="QuestionEditionModal__hints">
+                    <h4>Indices</h4>
+                    {hints?.length === 0 && (
+                    <p
+                      className="QuestionEditionModal__hints-empty"
+                    >
+                      Ajoutez des indices pour retrouver l&apos;information plus facilement
+                    </p>
+                    )}
+                    <ul>
+                      {hints?.map((existingHint) => (
+                        <li key={existingHint}>
+                          {existingHint}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <div className="QuestionEditionModal__actions">
+                  <div className="QuestionEditionModal__actions-left">
                     <Button
                       onClick={closeModal}
                       type="button"
                       variant="secondary"
                       text="Annuler"
+                    />
+                  </div>
+                  <div className="QuestionEditionModal__actions-right">
+                    <Button
+                      onClick={deleteCard}
+                      type="button"
+                      variant="danger"
+                      text="Supprimer"
                     />
                     <Button
                       type="submit"
@@ -146,7 +157,7 @@ export default function QuestionEditionModal(props: QuestionEditionModalProps) {
                     />
                   </div>
                 </div>
-              </>
+              </div>
             )}
           </div>
         </CSSTransition>
